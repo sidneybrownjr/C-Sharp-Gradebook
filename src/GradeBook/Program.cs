5a8de84c2f1745f19170448c716ea1bfab18185d
Sid <46009984+sidneybrownjr@users.noreply.github.com>
2020-11-21 11:42:00 -0600

    Initialize repo.

---
+++ src/GradeBook/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GradeBook
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IBook book = new DiskBook("Sidney's Grade Book");
+            book.GradeAdded += OnGradeAdded;
+
+
+            // input loop for grades
+            System.Console.WriteLine("Enter a grade or 'q' to quit.");
+            var input = Console.ReadLine();
+
+            input = EnterGrades(book, input);
+
+            var stats = book.GetStatistics();
+            
+            System.Console.WriteLine($"For the book named {book.Name}");
+            System.Console.WriteLine($"The average grade is {stats.Average:N1}");
+            System.Console.WriteLine($"The lowest grade is {stats.Low:N1}");
+            System.Console.WriteLine($"The highest grade is {stats.High:N1}");
+            System.Console.WriteLine($"The letter grade is {stats.Letter}");
+        }
+
+        private static string EnterGrades(IBook book, string input)
+        {
+
+            while (input != "q")
+            {
+                try
+                {
+                    var grade = double.Parse(input);
+                    book.AddGrade(grade);
+                }
+                catch (Exception ex)
+                {
+                    System.Console.WriteLine(ex.Message);
+                }
+
+                System.Console.WriteLine("Enter a grade or 'q' to quit.");
+                input = Console.ReadLine();
+            }
+
+            return input;
+        }
+
+        static void OnGradeAdded(object sender, EventArgs e)
+        {
+            System.Console.WriteLine("A grade was added.");
+        }
+    }
+}
---
+++ src/GradeBook/Statistics.cs
@@ -0,0 +1,60 @@
+using System;
+
+namespace GradeBook 
+{
+    public class Statistics
+    {
+        public Statistics () {
+            Count = 0;
+            Sum = 0.0;
+            High = double.MinValue;
+            Low = double.MaxValue;
+        }
+
+        public void Add(double number)
+        {
+            Sum += number;
+            Count += 1;
+            High = Math.Max(number, High);
+            Low = Math.Min(number, Low);
+
+        }
+
+        public double Average
+        {
+            get
+            {
+                return Sum / Count;
+            }
+        }
+        public double High;
+        public double Low;
+        public char Letter
+        {
+            get
+            {
+                switch(Average)
+                {
+                    case var d when d >= 90.0:
+                        return 'A';
+                        
+                    case var d when d >= 80.0:
+                        return  'B';
+                        
+                    case var d when d >= 70.0:
+                        return 'C';
+                        
+                    case var d when d >= 60.0:
+                        return  'D';
+                        
+                    default: 
+                        return  'F';
+                        
+                }
+
+            }
+        }
+        public double Sum;
+        public int Count; 
+    }
+}
